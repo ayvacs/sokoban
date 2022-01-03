1a58287fdb4e32ff28dbe2b408eb78976ac2b510
dom (@frogweezer) <dompec07@gmail.com>
2022-01-03 18:35:14 -0500

    Working on it

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Print instructions
+var instructions = `
+    *************************
+            Hi!
+    Welcome to Sokoban!
+    *************************
+
+    Your objective is to push the box
+    into the hole.
+
+    Type [w], [a], [s] and [d] to move
+\n`;
+
+console.log(instructions);
+
