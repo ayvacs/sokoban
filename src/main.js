dc22442e6a963c294e15b47aa0f664a15eec3af6
dom (@frogweezer) <dompec07@gmail.com>
2022-01-03 20:59:31 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 var newLine = " ";
 
+// Variables
+var windowEmoji = [
+    "⬛️",
+    "⬜"
+];
+
+// Configuration
+var config = {
+    windowLength: 16,
+    windowHeight: 9,
+
+    // Canvas height should be two larger than the
+    // respective window axis
+    canvasLength: 18,
+    canvasHeight: 11
+};
+
 // Functions
 function print(input) {
     if (typeof(input) == "object") {
@@ -10,22 +27,47 @@ function print(input) {
 
         console.log(str + "\n");
     } else {
+        input.replace("⬜"," ");
         var i = input.split("\n");
+        i.pop();
         i.pop();
         print(i);
     };
 };
 
+function generateWindow() {
+    var window = [];
+    // Generate rows
+    for (var x = 0; x <= config.canvasHeight; x++) {
+        window[x] = [];
+    };
+
+    // Generate border
+    // Generate border TOP
+    for (var x = 0; x < config.canvasLength; x++) {
+        window[0][x] = windowEmoji[1];
+    };
+
+    // Generate border BOTTOM
+    for (var x = 0; x < config.canvasLength; x++) {
+        window[config.canvasHeight - 1][x] = windowEmoji[1];
+    };
+
+    // Generate border LEFT
+    for (var x = 0; x < config.canvasHeight; x++) {
+        window[x][1] = windowEmoji[1];
+    }
+
+    // Generate border RIGHT
+    for (var x = 0; x < config.canvasHeight; x++) {
+        //window[x][0] = windowEmoji[1];
+    }
+
+    return window
+};
+
 function printWindow() {
-    var window = [
-        [
-            true,false
-        ], [
-            false
-        ], [
-            true,false,false,false,true
-        ]
-    ]
+    var window = generateWindow();
 
     var totalCell = "";
     window.forEach(function (row, i) {
@@ -36,9 +78,16 @@ function printWindow() {
                 emojiCode = 0;
             } else if (cell == true) {
                 emojiCode = 1;
+            } else {
+                emojiCode = cell;
             };
 
-            var emoji = windowEmoji[emojiCode];
+            var emoji;
+            if (windowEmoji[emojiCode]) {
+                emoji = windowEmoji[emojiCode];
+            } else {
+                emoji = emojiCode;
+            }
             currentRow += emoji;
         });
 
@@ -50,12 +99,6 @@ function printWindow() {
     });
     print(totalCell);
 };
-
-// Variables
-var windowEmoji = [
-    "⬛️",
-    "⬜"
-];
 
 print([
     "*************************",
