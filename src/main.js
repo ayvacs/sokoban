1104372692136d44ceb68b8bbff5786153f16c08
dom (@frogweezer) <dompec07@gmail.com>
2022-01-03 19:28:15 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,5 +10,43 @@ var instructions = `
 
     Type [w], [a], [s] and [d] to move
 \n`;
-
 console.log(instructions);
+
+// Variables
+var windowEmoji = [
+    "⬜️",
+    "⬛️"
+];
+
+// Functions
+function printWindow() {
+    var window = [
+        [
+            true,false
+        ], [
+            false
+        ]
+    ]
+
+    var totalCell = "";
+    window.forEach(function (row, _) {
+        var currentRow = "";
+        row.forEach(function (cell, i) {
+            var emojiCode;
+            if (cell == false) {
+                emojiCode = 0;
+            } else if (cell == true) {
+                emojiCode = 1;
+            };
+
+            var emoji = windowEmoji[emojiCode];
+            currentRow += emoji;
+        });
+
+        totalCell += currentRow + "\n";
+    });
+
+    console.log(totalCell);
+}
+
+printWindow();
